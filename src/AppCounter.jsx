1d80f278ba6ac7e465819c725d1ed3525ef465f4
dev-seolyu <dev.seolyu@gmail.com>
2023-05-26 15:29:17 +0900

    useState

---
+++ src/AppCounter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Counter from './components/Counter';
+
+function AppCounter() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default AppCounter;
