b2b5884921d5c1f41acff64c579e7c3a92286848
dev-seolyu <dev.seolyu@gmail.com>
2023-05-26 15:45:24 +0900

    Implement shared data and function in parent component and pass them as props to children

--- src/AppCounter.jsx
+++ src/AppCounter.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 
 function AppCounter() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount((prev) => prev + 1);
   return (
-    <div>
-      <Counter />
+    <div className="containter">
+      <div className="banner">
+        Total Count: {count} {count > 10 ? '❤️‍🔥' : '🤍'}
+      </div>
+      <div className="counters">
+        <Counter total={count} onClick={handleClick} />
+        <Counter total={count} onClick={handleClick} />
+      </div>
     </div>
   );
 }
